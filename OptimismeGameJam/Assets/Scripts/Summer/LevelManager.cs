fa0f0e3c565c900dcc00b3916b89c472894814a3
InsaneTomboy <sumxxlove@gmail.com>
2022-01-13 13:16:53 +0100

    Level manager working properly including level unlock

--- OptimismeGameJam/Assets/Scripts/Summer/LevelManager.cs
+++ OptimismeGameJam/Assets/Scripts/Summer/LevelManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,9 +10,16 @@ public class LevelManager : MonoBehaviour
 
     public static int lastLevel;
 
+    static List<Button> levelButtons;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        
     }
 
     public static void LoadLastLevel()
@@ -21,11 +29,13 @@ public class LevelManager : MonoBehaviour
     public static void LoadLevel(int currentL)
     {
         currentLevel = currentL;
+        levelButtons[currentLevel - 2].gameObject.SetActive(true);
         SceneManager.LoadScene(currentLevel);
     }
     public static void LoadNextLevel()
     {
         currentLevel++;
+        levelButtons[currentLevel - 2].gameObject.SetActive(true);
         SceneManager.LoadScene(currentLevel);
     }
     public static void LoadGameOver()
